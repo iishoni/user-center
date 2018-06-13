968a621249d282deafea60bafbaef6f76d91da84
dxk19911212 <648638730@qq.com>
2018-06-13 17:19:20 +0800

    修改

--- src/main/java/com/iishoni/usercenter/mapper/Generator.java
+++ src/main/java/com/iishoni/usercenter/mapper/Generator.java
@@ -9,21 +9,19 @@ import org.beetl.sql.core.db.MySqlStyle;
 import org.beetl.sql.ext.DebugInterceptor;
 
 public class Generator {
-
-    public static void main(String[] args) throws Exception{
-        String driver = "com.mysql.jdbc.Driver";
-        String url = "jdbc:mysql://mysql.iishoni.com/test";
-        String userName = "root";
-        String password = "root";
-        SQLManager sqlManager = new SQLManager(
-                new MySqlStyle(),
-                new ClasspathLoader("/sql"),
-                ConnectionSourceHelper.getSimple(driver, url, userName, password),
-                new UnderlinedNameConversion(),
-                new Interceptor[]{new DebugInterceptor()});
-
-        sqlManager.genPojoCodeToConsole("admin");
-        sqlManager.genSQLTemplateToConsole("admin");
-    }
-
+//    public static void main(String[] args) throws Exception{
+//        String driver = "com.mysql.jdbc.Driver";
+//        String url = "jdbc:mysql://mysql.iishoni.com/test";
+//        String userName = "root";
+//        String password = "root";
+//        SQLManager sqlManager = new SQLManager(
+//                new MySqlStyle(),
+//                new ClasspathLoader("/sql"),
+//                ConnectionSourceHelper.getSimple(driver, url, userName, password),
+//                new UnderlinedNameConversion(),
+//                new Interceptor[]{new DebugInterceptor()});
+//
+//        sqlManager.genPojoCodeToConsole("admin");
+//        sqlManager.genSQLTemplateToConsole("admin");
+//    }
 }
