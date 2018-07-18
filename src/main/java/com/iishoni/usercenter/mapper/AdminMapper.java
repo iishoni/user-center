e91189084762752319e62c977a1da2b80105e1d8
dxk19911212 <daixiaokang@xiaoweiba.com.cn>
2018-07-18 18:13:22 +0800

    小修小改[ci skip]

--- src/main/java/com/iishoni/usercenter/mapper/AdminMapper.java
+++ src/main/java/com/iishoni/usercenter/mapper/AdminMapper.java
@@ -2,9 +2,7 @@ package com.iishoni.usercenter.mapper;
 
 import com.iishoni.usercenterapi.model.Admin;
 
-import org.beetl.sql.core.annotatoin.Param;
 import org.beetl.sql.core.mapper.BaseMapper;
 
 public interface AdminMapper extends BaseMapper<Admin> {
-
 }
